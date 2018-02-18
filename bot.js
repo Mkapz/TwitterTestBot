aff142a329340a5cd976fbc335ec261c656c1e9b
unknown <mkap25@yahoo.com>
2018-02-18 01:19:23 -0500

    first push able to retweet

---
+++ bot.js
@@ -0,0 +1,38 @@
+console.log("the bot is starting");
+
+var Twit= require('twit');
+var config= require('./config.js');
+
+var T= new Twit(config);
+
+var params = {
+  q: '#nodejs',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+
+ T.get('search/tweets', params, function(err, data) {
+      // if there no errors
+        if (!err) {
+          // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            T.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong while SEARCHING...');
+        }
+    });
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:'pPFEqK7qV2nNtO9Ml7CNISGCw',  
+  consumer_secret:'S7GNbyz7XsSRyoNxOfRRoH8i4IMjcd4csg2n0GX0Db7ryJe76e',
+  access_token:'838579399150694400-yn0HOnyN1TixpslZ4wD6nxkGiJH94yw',  
+  access_token_secret:'mLGwfLdHotcv9ulWMc7YdvQKKVZzKpQJHckffm7Cjp3iN'
+}
